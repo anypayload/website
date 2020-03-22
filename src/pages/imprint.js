14184e2475ca0066d176fbea26cefafa2422c8b7
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2020-03-22 08:08:51 +0100

    Add imprint which is required by law

---
+++ src/pages/imprint.js
@@ -0,0 +1,128 @@
+import React from "react"
+import { Container, Row, Col } from "reactstrap"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ImprintPage = () => (
+  <Layout className="bg-light">
+    <SEO 
+      title="Imprint"
+    />
+
+    <header className="py-5">
+      <Container>
+        <Row className="justify-content-center">
+          <Col md="6" className="text-center">
+            <h1>Imprint</h1>
+            <h2 className="text-muted lead mt-4">
+              Legal notices about this service.
+            </h2>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+
+    <section className="bg-white py-5">
+      <Container>
+        <Row className="align-items-center pt-5 pt-lg-7">
+          <Col>
+            <h2>Datenschutz</h2>
+          </Col>
+        </Row>
+        <Row className="py-5 pt-lg-7 pb-lg-5">
+          <Col md="4">
+            <h5>Anbieter und Verantwortlicher</h5>
+            <address>
+              Thomas Schmidleithner, BSc<br />
+              Meggenhofen 95/11<br />
+              4714 Meggenhofen<br />
+              Austria
+            </address>
+          </Col>
+          <Col md="4">
+            <h5>Telefon</h5>
+            <p>+43 664 14 555 90</p>
+          </Col>
+          <Col md="4">
+            <h5>Email</h5>
+            <p>
+              <a href="mailto:info@serverless.at">info@serverless.at</a>
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2 className="mb-3">Arten der erhobenen Daten</h2>
+            <p>Zu den personenbezogenen Daten, die diese Anwendung selbstständig oder durch Dritte verarbeitet, gehören: Firma/Projektname, E-Mail, Nachricht, Budgetrahmen, Cookie und Nutzungsdaten.</p>
+
+            <p>
+            Vollständige Details zu jeder Art von verarbeiteten personenbezogenen Daten werden in den dafür vorgesehenen Abschnitten dieser Datenschutzerklärung oder durch konkrete Erklärungstexte bereitgestellt, die vor der Datenverarbeitung angezeigt werden.
+            Die personenbezogenen Daten können vom Nutzer freiwillig bereitgestellt oder, im Falle von Nutzungsdaten, automatisch verarbeitet werden, wenn diese Anwendung genutzt wird.
+            Alle durch diese Anwendung angeforderten Daten sind obligatorisch. Sollten diese Daten nicht bereitgestellt werden, kann dies dazu führen, dass diese Anwendung nicht in der Lage ist, ihre Dienste zur Verfügung zu stellen. In Fällen, in denen diese Anwendung ausdrücklich angibt, dass einige Daten nicht obligatorisch sind, dürfen sich die Nutzer dafür entscheiden, diese Daten ohne jegliche Folgen für die Verfügbarkeit oder die Funktion des Dienstes nicht weiterzugeben.
+            Nutzer, die sich darüber im Unklaren sind, welche personenbezogenen Daten obligatorisch sind, können gerne den Eigentümer kontaktieren.
+            Jegliche Verwendung von Cookies - oder anderer Tracking-Tools - durch diese Anwendung oder die Besitzer von Drittdiensten, die durch diese Anwendung genutzt werden, dient dem Zweck, den vom Nutzer gewünschten Dienst bereitzustellen, und allen anderen Zwecken, die im vorliegenden Dokument und, falls vorhanden, in der Cookie-Richtlinie beschrieben sind.
+            </p>
+
+            <p>Die Nutzer sind für alle personenbezogenen Daten Dritter verantwortlich, die durch diese Anwendung beschafft, veröffentlicht oder weitergegeben werden, und bestätigen, dass sie die Zustimmung zur Übermittlung personenbezogener Daten etwaiger Dritter an diese Anwendung eingeholt haben.</p>
+
+            <h2 className="mt-5">Art und Ort der Datenverarbeitung</h2>
+            <h3>Verarbeitungsmethoden</h3>
+            <p>Die verantwortliche Stelle verarbeitet die Nutzerdaten auf ordnungsgemäße Weise und ergreift angemessene Sicherheitsmaßnahmen, um den unbefugten Zugriff und die unbefugte Weiterleitung, Modifizierung oder Vernichtung von Daten zu vermeiden.<br />Die Datenverarbeitung wird mittels Computern oder IT-basierten Systemen durchgeführt, einem organisatorischen Verfahren und Modus folgend die strikt auf die angegebenen Zwecke abzielen. Neben der verantwortlichen Stelle wird möglicherweise auch bestimmten Kategorien von Verantwortlichen Zugriff auf die Daten gewährt, die an der Betreibung der Website beteiligt sind (Personalverwaltung, Vertrieb, Marketing, Recht, Systemadministratoren) oder externen Parteien (wie Fremdanbietern technischer Dienstleistungen, Zustellunternehmen, Hosting-Anbietern, IT-Unternehmen oder Kommunikationsagenturen), die, falls notwendig, vom Eigentümer als Auftragsverarbeiter eingesetzt werden. Eine aktuelle Liste dieser Beteiligten kann jederzeit von der verantwortlichen Stelle verlangt werden.</p>
+            
+            <h3>Ort</h3>
+            <p>Die Daten werden am Betriebsort der verantwortlichen Stelle und an allen anderen Orten, an denen sich die mit der Datenverarbeitung beteiligten Parteien befinden, verarbeitet. Für weitere Informationen, kontaktieren Sie bitte die verantwortliche Stelle.</p>
+            
+            <h3>Speicherdauer</h3>
+            <p>Die Daten werden so lange gespeichert, wie es für das Erbringen der vom Nutzer gewünschten Dienstleistungen notwendig ist oder für die in diesem Dokument aufgeführten Zwecke angegeben ist. Der Nutzer kann die verantwortliche Stelle jederzeit auffordern, die Daten zu sperren oder zu entfernen.</p>
+
+            <h2 className="mt-5">Nutzung der erhobenen Daten</h2>
+            <p> Die Daten über den Nutzer werden erhoben, damit der Eigentümer die Dienstleistungen erbringen kann. Darüber hinaus werden Daten zu folgenden Zwecken erhoben: Kontaktieren des Nutzers, Interaktion mit externen sozialen Netzwerken und Plattformen, Analytik, Verwaltung von Nutzer-Datenbanken, Testen der Performance von Inhalten und Funktionen (A/B-Test), Kommentarfunktion und Erstellen und Verwalten von Sicherungskopien.</p>
+            <p>Die personenbezogenen Daten, die zu den aufgeführten Zwecken verwendet werden, sind in den jeweiligen Abschnitten dieses Dokuments aufgeführt.</p>
+
+            <h2 className="mt-5">Nutzungsdaten</h2>
+            <p>Informationen, die diese Anwendung (oder Dienste Dritter, die diese Anwendung in Anspruch nimmt), automatisch erhebt, z. B.: die IP-Adressen oder Domain-Namen der Computer von Nutzern, die diese Anwendung verwenden, die URI-Adressen (Uniform Resource Identifier), den Zeitpunkt der Anfrage, die Methode, die für die Übersendung der Anfrage an den Server verwendet wurde, die Größe der empfangenen Antwort-Datei, der Zahlencode, der den Status der Server-Antwort anzeigt (erfolgreiches Ergebnis, Fehler etc.), das Herkunftsland, die Funktionen des vom Nutzer verwendeten Browsers und Betriebssystems, die diversen Zeitangaben pro Aufruf (z. B. wie viel Zeit auf jeder Seite der Anwendung verbracht wurde) und Angaben über den Pfad, dem innerhalb einer Anwendung gefolgt wurde, insbesondere die Reihenfolge der besuchten Seiten, sowie sonstige Informationen über das Betriebssystem des Geräts und/oder die IT-Umgebung des Nutzers.</p>
+
+            <h2 className="mt-5">Ihre Rechte</h2>
+            <p>Ihnen stehen grundsätzlich die Rechte auf Auskunft, Berichtigung, Löschung, Einschränkung, Datenübertragbarkeit, Widerruf und Widerspruch zu. Wenn Sie glauben, dass die Verarbeitung Ihrer Daten gegen das Datenschutzrecht verstößt oder Ihre datenschutzrechtlichen Ansprüche sonst in einer Weise verletzt worden sind, können Sie sich bei der Aufsichtsbehörde beschweren. In Österreich ist dies die Datenschutzbehörde, deren Webseiten Sie unter <a href="https://www.dsb.gv.at/" class="external" rel="nofollow">https://www.dsb.gv.at/</a> finden.</p>
+
+            <h2 className="mt-5">SSL- bzw. TLS-Verschlüsselung</h2>
+            <p>Wir verwenden zum Schutz Ihrer personenbezognene Daten und anderer vertraulicher Inhalte eine TLS- bzw. SSL-Verschlüsselung. Sie erkennen die Benutzung dieser Absicherung der Datenübertragung am kleinen Schloßsymbol oben im Browser und der Verwendung des Schemas https als Teil unserer Internetadresse.</p>
+
+            <h2 className="mt-5">Cookies</h2>
+            <p>Unsere Website verwendet HTTP-Cookies um nutzerspezifische Daten zu speichern.</p>
+            Ein Cookie ist ein kurzes Datenpaket, welches zwischen Webbrowser und Webserver ausgetauscht wird, für diese aber völlig bedeutungslos ist und erst für die Webanwendung, z. B. einen Online-Shop, eine Bedeutung erhält, etwa den Inhalt eines virtuellen Warenkorbes.<br />
+            Es gibt zwei Arten von Cookies: Erstanbieter-Cookies werden von unserer Website erstellt, Drittanbieter-Cookies werden von anderen Websites (z. B. Google Analytics) erstellt.<br />
+            Man unterscheidet drei Kategorien von Cookies: unbedingt notwendige Cookies um grundlegende Funktionen der Website sicherzustellen, funktionelle Cookies um die Leistung der Webseite sicherzustellen und zielorientierte Cookies um das Benutzererlebnis zu verbessern.<br />
+            Wir nutzen Cookies, um unsere Webseite nutzerfreundlicher zu gestalten. Einige Cookies bleiben auf Ihrem Endgerät gespeichert, bis Sie diese löschen. Sie ermöglichen es uns, Ihren Browser beim nächsten Besuch wiederzuerkennen.<br />
+            <p>Wenn Sie dies nicht wünschen, so können Sie Ihren Browser so einrichten, dass er Sie über das Setzen von Cookies informiert und Sie dies nur im Einzelfall erlauben.<br />
+            Sie können jederzeit Cookies, die sich bereits auf Ihrem Computer befinden, löschen oder Cookies deaktivieren. Die Vorgangsweise dazu ist nach Browser unterschiedlich, am besten Sie suchen die Anleitung in Google mit dem Suchbegriff „cookies löschen chrome“ oder „cookies deaktivieren chrome“ im Falle eines Chrome Browsers oder tauschen das Wort „chrome“ gegen den Namen Ihres Browsers, z. B. edge, firefox, safari aus.<br />
+            Wenn Sie uns generell nicht gestatten, Cookies zu nutzen, d.h. diese per Browsereinstellung deaktivieren, können manche Funktionen und Seiten nicht wie erwartet funktionieren.</p>
+
+            <h2 className="mt-5">Google Analytics Datenschutzerklärung</h2>
+            <p>Wir verwenden auf dieser Webseite Google Analytics der Firma Google Inc. (1600 Amphitheatre Parkway Mountain View, CA 94043, USA) um Besucherdaten statistisch auszuwerten. Dabei verwendet Google Analytics zielorientierte Cookies.</p>
+            <p>Nähere Informationen zu Nutzungsbedingungen und Datenschutz finden Sie unter <a href="http://www.google.com/analytics/terms/de.html" class="external" rel="nofollow">http://www.google.com/analytics/terms/de.html</a> bzw. unter <a href="https://support.google.com/analytics/answer/6004245?hl=de" class="external" rel="nofollow">https://support.google.com/analytics/answer/6004245?hl=de</a>.</p>
+
+            <h3>Pseudonymisierung</h3>
+            <p>Unser Anliegen im Sinne der DSGVO ist die Verbesserung unseres Angebotes und unseres Webauftritts. Da uns die Privatsphäre unserer Nutzer wichtig ist, werden die Nutzerdaten pseudonymisiert.&nbsp;Die Datenverarbeitung erfolgt auf Basis der gesetzlichen Bestimmungen des § 96 Abs 3 TKG sowie des Art 6 EU-DSGVO Abs 1 lit a (Einwilligung) und/oder f (berechtigtes Interesse) der DSGVO.</p>
+
+            <h3>Deaktivierung der Datenerfassung durch Google Analytics</h3>
+            <p>Mithilfe des <strong>Browser-Add-ons zur Deaktivierung</strong> von Google Analytics-JavaScript (ga.js, analytics.js, dc.js) können Website-Besucher verhindern, dass Google Analytics ihre Daten verwendet.</p>
+            <p>Sie können die Erfassung der durch das Cookie erzeugten und auf Ihre Nutzung der Website bezogenen Daten an Google sowie die Verarbeitung dieser Daten durch Google verhindern, indem Sie das unter dem folgenden Link verfügbare Browser-Plugin herunterladen und installieren: <a href="https://tools.google.com/dlpage/gaoptout?hl=de" class="external" rel="nofollow">https://tools.google.com/dlpage/gaoptout?hl=de</a>.</p>
+
+            <h2 className="mt-5">Google Analytics IP-Anonymisierung</h2>
+            <p>Wir haben auf dieser Webseite die IP-Adressen-Anonymisierung von Google Analytics implementiert.&nbsp;Diese Funktion wurde von Google entwickelt, damit diese Webseite die geltenden Datenschutzbestimmungen und Empfehlungen der lokalen Datenschutzbehörden einhalten kann, wenn diese eine Speicherung der vollständigen IP-Adresse untersagen. Die Anonymisierung bzw. Maskierung der IP findet statt, sobald die IP-Adressen im Google Analytics-Datenerfassungsnetzwerk eintreffen und bevor eine Speicherung oder Verarbeitung der Daten stattfindet.</p>
+            <p>Mehr Informationen zur IP-Anonymisierung finden Sie auf&nbsp;<a href="https://support.google.com/analytics/answer/2763052?hl=de" target="_blank" rel="noopener noreferrer" class="external">https://support.google.com/analytics/answer/2763052?hl=de</a>.</p>
+
+            <h2 className="mt-5">Google Fonts Datenschutzerklärung</h2>
+            <p>Wir verwenden Google Fonts der Firma Google Inc. (1600 Amphitheatre Parkway Mountain View, CA 94043, USA) auf unserer Webseite. Die Verwendung von Google Fonts erfolgt ohne Authentisierung und es werden keine Cookies and die Google Fonts API gesendet. Sollten Sie ein Konto bei Google haben, werden keine Ihrer Google-Kontodaten an Google während der Nutzung von Google Fonts übermittelt. Google erfasst lediglich die Nutzung von CSS und der verwendeten Fonts und speichert diese Daten sicher. Mehr zu diesen und weiteren Fragen finden Sie auf <a href="https://developers.google.com/fonts/faq" class="external" rel="nofollow">https://developers.google.com/fonts/faq</a>.</p>
+            <p>Welche Daten von Google erfasst werden und wofür diese Daten verwendet werden, können Sie auf <a href="https://www.google.com/intl/de/policies/privacy/" class="external" rel="nofollow">https://www.google.com/intl/de/policies/privacy/</a> nachlesen.</p>
+
+            <i className="small">Quelle: Teilweise adaptiert vom Datenschutz Generator von firmenwebseiten.at</i>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  </Layout>
+)
+
+export default ImprintPage

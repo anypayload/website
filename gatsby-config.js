21b5a977700e89f66aa4ae8f618ff5520c4866ba
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2020-04-05 14:26:15 +0200

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,17 @@ module.exports = {
       },
     },
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-11707735-18",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

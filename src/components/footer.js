0b411cd615f748eaa730c9001e6b6d8fe43cbb33
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2020-08-10 21:32:17 +0200

    Add social footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from "gatsby"
 import { Container, Row, Col, Nav, NavItem, NavLink } from 'reactstrap'
+import { FaLinkedin } from "react-icons/fa"
 import LogoBlack from '../images/anypayload-black.png'
 /*import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faMeetup from '@fortawesome/fontawesome-free-brands/faMeetup'
@@ -10,18 +11,7 @@ const Footer = () =>
   <footer className="pb-5">
     <Container>
       <Row className="pb-5 align-items-centers">
-        {/*<Col xs="6" md="3" className="pt-5">
-          <Nav vertical>
-            <NavItem>
-              <NavLink tag={Link} to="/changelog/" className="text-uppercase text-muted">Payloads</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink tag={Link} to="/changelog">Changelog</NavLink>
-            </NavItem>
-          </Nav>
-</Col>*/}
-
-        <Col xs="6" md="4" className="pt-5">
+        <Col xs="6" md="3" className="pt-5">
           <Nav vertical>
             <NavItem>
               <NavLink to="/products/" tag={Link} className="text-uppercase text-muted">Products</NavLink>
@@ -38,7 +28,7 @@ const Footer = () =>
           </Nav>
         </Col>
 
-        <Col xs="6" md="4" className="pt-5">
+        <Col xs="6" md="3" className="pt-5">
           <Nav vertical>
             <NavItem>
               <NavLink to="/contact/" tag={Link} className="text-uppercase text-muted">Contact</NavLink>
@@ -49,7 +39,20 @@ const Footer = () =>
           </Nav>
         </Col>
 
-        <Col xs="12" md="4" className="text-center text-md-right pt-5">
+        <Col xs="6" md="3" className="pt-5">
+          <Nav vertical>
+            <NavItem>
+              <NavLink to="/contact/" tag={Link} className="text-uppercase text-muted">Social</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="https://www.linkedin.com/company/anypayload" tag={Link}>
+                <FaLinkedin className="h4" />
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Col>
+
+        <Col xs="12" md="3" className="text-center text-md-right pt-5">
           <img src={LogoBlack} className="img-fluid mx-md-n1 w-50 mt-5 mt-md-0" alt="anypayload Logo" />
           <small className="mt-md-3 d-block">Production-ready sensor payloads for your IoT business.</small>
         </Col>

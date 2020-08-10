ec67a30455110232a96234355edc426496618359
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2020-08-10 21:37:46 +0200

    Update development

--- src/pages/development.js
+++ src/pages/development.js
@@ -73,8 +73,8 @@ class Decoder extends React.Component {
                     <option value="elsys">Elsys</option>
                     <option value="elvaco-CMi4160">Elvaco CMi4160</option>
                     <option value="gavazzi-R56">Gavazzi R56 (in development)</option>
-                    <option value="libelium-smart-parking-v1">Libelium Smart Parking (v1)</option>
-                    <option value="ursalink-AM100">Ursalink AM100/AM102 series (in development)</option>
+                    <option value="libelium-smart-parking-v1">Libelium Smart Parking V1</option>
+                    <option value="ursalink-AM100">Ursalink AM100/AM102 series</option>
                   </Input>
                 </FormGroup>
                 <FormGroup>

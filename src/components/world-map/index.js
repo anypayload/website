21dae66747942b9e38027d008dc72e95a3362123
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2021-04-05 11:26:20 +0200

    Improve styling for about page

--- src/components/world-map/index.js
+++ src/components/world-map/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import map from './map.svg'
 
 const WorldMap = ({ children, className }) =>
-  <div className={className} style={{ backgroundImage: `url(${map})` }}>
+  <div className={className} style={{ backgroundImage: `url(${map})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
     {children}
   </div>
 
--- src/pages/about.js
+++ src/pages/about.js
@@ -17,37 +17,37 @@ const About = ({ data }) => (
       We are a small team located in Austria engaging in software consulting in IoT and related areas."
     />
 
-    <section>
-      <WorldMap className="py-md-5">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md="6" className="text-center">
-              <h1>About</h1>
-              <blockquote className="blockquote mt-4">
-                <p>
-                  We seek to simplify the use of IoT sensors and devices in production by eliminating repetitive tasks.
-                  We are a small team located in Austria engaging in software consulting in IoT and related areas.
-                </p>
-                <p>
-                  Feel free to contact us for more information and interesting engagements.
-                </p>
-              </blockquote>
-            </Col>
-          </Row>
 
+    <header className="py-5">
+      <Container>
+        <Row className="justify-content-center">
+          <Col md="6" className="text-center">
+            <h1>About</h1>
+            <h2 className="text-muted lead mt-4 lh-base">
+              We seek to simplify the use of IoT sensors and devices in production by eliminating repetitive tasks.
+              We are a small team located in Austria engaging in software consulting in IoT and related areas.
+            </h2>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+
+    <section>
+      <Container>
+        <WorldMap className="py-md-5">
           <Row className="mt-md-5 justify-content-center">
-            <Col md="5" className="mt-4 text-center text-md-start">
+            <Col md="4" className="mt-4 text-center text-md-start">
               <h6 className="text-uppercase text-primary">Our Mission</h6>
               <h3 className="pb-4">Make adoption of IoT technology simple.</h3>
             </Col>
 
-            <Col md={{ size: 5, offset: 1 }} className="mt-4 text-center text-md-start">
+            <Col md={{ size: 4, offset: 2 }} className="mt-4 text-center text-md-start">
               <h6 className="text-uppercase text-primary">About Us</h6>
               <h3 className="pb-4">We are a small team of software engineers and product managers embracing IoT technologies.</h3>
             </Col>
           </Row>
-        </Container>
-      </WorldMap>
+        </WorldMap>
+      </Container>
     </section>
 
     <section className="py-5">
@@ -95,6 +95,11 @@ const About = ({ data }) => (
               <ListGroupItem title="Github" tag={OutboundLink} target="_blank" href="https://github.com/tschmidleithner"><FaGithub /></ListGroupItem>
               <ListGroupItem title="Stack Overflow" tag={OutboundLink} target="_blank" href="https://stackoverflow.com/users/2571938/tom"><FaStackOverflow /></ListGroupItem>
             </ListGroup>
+          </Col>
+        </Row>
+        <Row className="justify-content-center text-center my-md-5">
+          <Col md="8">
+            <h4>Feel free to contact us for more information and interesting engagements.</h4>
           </Col>
         </Row>
       </Container>

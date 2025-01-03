f3a47465a92ef98252904eba332e168cb168c72d
Thomas Schmidleithner <thomas.schmidleithner@gmail.com>
2025-01-03 16:50:41 +0100

    Fix decoder.

--- netlify/functions/decoder.mjs
+++ netlify/functions/decoder.mjs
@@ -1,22 +1,15 @@
 import '../../decoder/axioma-qalcosonic-w1/javascript/decoder.js';
 
-exports.handler = async function(event, context) {
+export default async (req, context) => {
+    console.log("payload", req.body.payload)
     switch (context.params.device) {
         case "axioma-qalcosonic-w1":
-            const payload = hexToBytes(event.body.payload);
-            const port = event.body.port;
+            const payload = hexToBytes(req.body.payload);
+            const port = req.body.port;
             decoded = doDecode(payload, port);
-            return {
-                statusCode: 200,
-                body: JSON.stringify(decoded)
-            };
+            return Response.json(decoded, { status: 200 });
         default:
-            return {
-                statusCode: 404,
-                body: JSON.stringify({
-                    error: "Device not found"
-                })
-            }
+            return new Response("Unknown decoder", { status: 404 });
     }
 };
 
